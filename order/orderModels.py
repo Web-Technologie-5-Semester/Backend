be00a3320609bd714d65af55ccd2839b1ea3c8b5
Lorenzo <maximilian_h@outlook.com>
2024-11-25 10:43:59 +0100

    isbn statt product_id
    Co-authored-by: Rebeka <131900433+rebekafischer@users.noreply.github.com>

--- order/orderModels.py
+++ order/orderModels.py
@@ -27,7 +27,7 @@ class Order_Item(SQLModel, table=True):
     unique_order_item_id: int | None = Field(default=None, primary_key=True)
     
     unique_order_id: int = Field(foreign_key="order.unique_order_id")  
-    product_id: str = Field(foreign_key="book.isbn")  
+    isbn: str = Field(foreign_key="book.isbn")  
     
     quantity: int = Field(default=1)
     price: float = Field()

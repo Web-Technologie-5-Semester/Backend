a19036636371e2cb47e31c90b8f94a44396d3f90
Rebeka Fischer <rebeka.fischer@outlook.de>
2024-12-07 15:15:40 +0100

    new seller table

--- user/models.py
+++ user/models.py
@@ -3,12 +3,29 @@ from sqlalchemy import Column, String
 from sqlmodel import Field, Session, SQLModel, create_engine, select, Relationship
 
 
+class Role(SQLModel, table= True):
+
+    id: int | None = Field(default= None, primary_key= True)
+    role: str = Field()
+    users: list["User"] = Relationship(back_populates="role")
+
+
+class Seller(SQLModel, table=True):
+
+    user_id: int = Field(primary_key=True, foreign_key="user.id")
+    bank: str = Field()
+    BIC: int = Field()
+    banking_name: str = Field()
+    IBAN: str = Field()
+    sales_tax_id: int = Field()
+
+    user: list["User"] = Relationship(back_populates="seller")
 
 #User
 class User(SQLModel, table = True):
 
     id: int | None = Field(default= None, primary_key= True)
-    forename: str = Field()
+    first_name: str = Field()
     name: str = Field()
     email: str = Field()
     street: str = Field()
@@ -18,28 +35,14 @@ class User(SQLModel, table = True):
     postal_code: int = Field()
     birthday: str = Field()
     password_hash: str = Field()
-    role: str = Field(sa_column=Column(String, nullable=False))
+    role_id: int = Field(foreign_key="role.id")
 
-    def __innit__(self, **kwargs):
-        super().__init__(**kwargs)
-        if not getattr(self, "role", None):
-            self.role = "User"
-   
+    role: Role = Relationship(back_populates="users")
+    seller: Seller = Relationship(back_populates="user")
 
-class Customer(User):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.role = "Customer"
+
+
     
-
-class Seller(User):
-
-    IBAN: str = Field()
-    sales_tax_id: int = Field()
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.role = "Seller"
 
 
 class Token(BaseModel):

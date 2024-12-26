f33c6927fbd1ccf6b4db99df4782119853a4ed7f
Rebeka Fischer <rebeka.fischer@outlook.de>
2024-12-26 16:59:48 +0100

    authentication with locked endpoints ready

--- routers/inventory.py
+++ routers/inventory.py
@@ -23,11 +23,6 @@ async def get_book_by_isbn(isbn: str, session: Session = Depends(get_session)):
     book= BookService(session).get_book_by_isbn(isbn)
     return book
 
-# @inv_router.delete("/book/{isbn}")
-# async def delete_book_by_isbn(isbn: str, _: Annotated[bool, Depends(RoleChecker(allowed_roles=[SELLER_ROLE]))], session: Session = Depends(get_session)): 
-#     return BookService(session).delete_book_by_isbn(isbn)
-
-
 @inv_router.delete("/book/{isbn}")
 async def delete_book_by_isbn(
     isbn: str,
@@ -37,12 +32,19 @@ async def delete_book_by_isbn(
     return BookService(session).delete_book_by_isbn(isbn)
 
 
-# @inv_router.post("/book", response_model=BookResponse)
-# async def create_book(book: BookCreate, _: Annotated[bool, Depends(RoleChecker(allowed_roles=[SELLER_ROLE]))], session: Session = Depends(get_session)):
-#     return BookService(session).create(book)
+@inv_router.post("/book", response_model=BookResponse)
+async def create_book(
+    book: BookCreate,
+    _: Annotated[bool, Depends(role_checker_factory(allowed_roles=[SELLER_ROLE]))], 
+    session: Session = Depends(get_session)):
+    return BookService(session).create(book)
                                  
 @inv_router.put("/book/{isbn}", response_model=Book)
-async def update_book(isbn: str, new_book :Book, session: Session = Depends(get_session)):
+async def update_book(
+    isbn: str, 
+    new_book :Book, 
+    _: bool = Depends(role_checker_factory(allowed_roles=[SELLER_ROLE])),
+    session: Session = Depends(get_session)):
     return BookService(session).update(isbn, new_book)
                                  
 #such endpunkt
@@ -61,15 +63,25 @@ async def get_books_by_author(author_id:int, session: Session = Depends(get_sess
     return AuthorService(session).get_books_by_author(author_id)
 
 @inv_router.delete("/author/{author_id}", response_model=Author)
-async def delete_author_by_id(id: int, session: Session = Depends(get_session)): 
+async def delete_author_by_id(
+    id: int, 
+    _: bool = Depends(role_checker_factory(allowed_roles=[SELLER_ROLE])),
+    session: Session = Depends(get_session)): 
     return AuthorService(session).delete_author_by_id(id) 
 
 @inv_router.post("/author", response_model=AuthorResponse)
-async def create_author(author: AuthorCreate, session: Session = Depends(get_session)):
+async def create_author(
+    author: AuthorCreate, 
+    _: bool = Depends(role_checker_factory(allowed_roles=[SELLER_ROLE])),
+    session: Session = Depends(get_session)):
     return AuthorService(session).create(author)
 
 @inv_router.put("/author/{author_id}", response_model=Author)
-async def update_author(id: int, new_author: Author, session: Session = Depends(get_session)):
+async def update_author(
+    id: int, 
+    new_author: Author, 
+    _: bool = Depends(role_checker_factory(allowed_roles=[SELLER_ROLE])),
+    session: Session = Depends(get_session)):
     return AuthorService(session).update(id, new_author)
 
 
@@ -83,15 +95,25 @@ async def get_books_by_genre(genre_id: int, session: Session = Depends(get_sessi
     return GenreService(session).get_books_by_genre(genre_id)
 
 @inv_router.delete("/genre/{genre_id}", response_model=Genre)
-async def delete_genre_by_id(id: int, session: Session = Depends(get_session)): 
+async def delete_genre_by_id(
+    id: int,
+    _: bool = Depends(role_checker_factory(allowed_roles=[SELLER_ROLE])),
+    session: Session = Depends(get_session)): 
     return GenreService(session).delete_genre_by_id(id) 
 
 @inv_router.post("/genre", response_model=GenreResponse)
-async def create_genre(genre: GenreCreate, session: Session = Depends(get_session)):
+async def create_genre(
+    genre: GenreCreate, 
+    _: bool = Depends(role_checker_factory(allowed_roles=[SELLER_ROLE])),
+    session: Session = Depends(get_session)):
     return GenreService(session).create(genre)
 
 @inv_router.put("/genre/{genre_id}", response_model=Genre)
-async def update_genre(id: int, new_genre: Genre, session: Session = Depends(get_session)):
+async def update_genre(
+    id: int, 
+    new_genre: Genre, 
+    _: bool = Depends(role_checker_factory(allowed_roles=[SELLER_ROLE])),
+    session: Session = Depends(get_session)):
     return GenreService(session).update(id, new_genre)
 
 
@@ -105,13 +127,23 @@ async def get_books_by_publisher(publisher_id: int, session: Session = Depends(g
     return PublisherService(session).get_books_by_publisher(publisher_id)
 
 @inv_router.delete("/publisher/{publisher_id}", response_model=Publisher)
-async def delete_publisher_by_id(id: int,  session: Session = Depends(get_session)): 
+async def delete_publisher_by_id(
+    id: int, 
+    _: bool = Depends(role_checker_factory(allowed_roles=[SELLER_ROLE])),
+    session: Session = Depends(get_session)): 
     return PublisherService(session).delete_publisher_by_id(id) 
 
 @inv_router.post("/publisher", response_model=PublisherResponse)
-async def create_publisher(publisher: PublisherCreate, session: Session = Depends(get_session)):
+async def create_publisher(
+    publisher: PublisherCreate, 
+    _: bool = Depends(role_checker_factory(allowed_roles=[SELLER_ROLE])),
+    session: Session = Depends(get_session)):
     return PublisherService(session).create(publisher)
 
 @inv_router.put("/publisher/{publisher_id}", response_model=Publisher)
-async def update_publisher(id: int, new_publisher: Publisher, session: Session = Depends(get_session)):
+async def update_publisher(
+    id: int, 
+    new_publisher: Publisher, 
+    _: bool = Depends(role_checker_factory(allowed_roles=[SELLER_ROLE])),
+    session: Session = Depends(get_session)):
     return PublisherService(session).update(id, new_publisher)

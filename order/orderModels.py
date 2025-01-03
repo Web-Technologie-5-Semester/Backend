95a52aa4ef5a97584fd6f10afb8861bf5850ff22
Lorenzo <maximilian_h@outlook.com>
2025-01-03 20:19:53 +0100

    ffff

--- order/orderModels.py
+++ order/orderModels.py
@@ -11,44 +11,27 @@ from .orderCRUD import StatusEnum
 class Order(SQLModel, table=True):
     unique_order_id: int | None = Field(default=None, primary_key=True)
     
-    # user_id: int = Field(foreign_key="user.id")  
     user_id: str = Field()  
-    
     created_at: date = Field(default=date.today)
     status: StatusEnum = Field()
     
-    items: List["Order_Item"] = Relationship(back_populates="order")  
     
-
+    
     
 class Order_Item(SQLModel, table=True):
-    # wird beim erstellen vergeben
     unique_order_item_id: int | None = Field(default=None, primary_key=True)
-    
-    
-    
-    # Wichtig, kommt als response von order_create zurück
+
     unique_order_id: int = Field(foreign_key="order.unique_order_id")
-    
-    # sollten in der liste stehen 
-    product_id: str = Field(foreign_key="book.isbn")  
+    product_id: str = Field()
+    name: str = Field()
+    image: str = Field(default = None)
     quantity: int = Field(default=1)
     price: float = Field()
-    
-    
-    # unwichtig
-    order: Order = Relationship(back_populates="items")  
 
 
 
 
 
-
-
-#TODO: Order create gehört zu models 
-
-
-    
     
     
     

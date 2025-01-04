11380f666c5d36d3be8dd8b55b44f723aa1c27c2
Rebeka Fischer <rebeka.fischer@outlook.de>
2025-01-04 12:01:09 +0100

    Port

--- main.py
+++ main.py
@@ -68,5 +68,5 @@ async def forbidden_handler(request: Request, exc: ExistingException):
 ####################################################################
 
 if __name__=="__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
